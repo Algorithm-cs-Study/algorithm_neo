0641d82f94578f57ec3f07a8b7d1d2e886474b38
ffolabear <ffolabear@gmail.com>
2023-02-18 23:00:21 +0900

    feat: 나누어 떨어지는 숫자 배열 풀었음

---
+++ src/programmers/lv1/Solution7.java
@@ -0,0 +1,28 @@
+package programmers.lv1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution7 {
+
+    public int[] solution(int[] arr, int divisor) {
+
+        List<Integer> answerList = new ArrayList<>();
+
+        for (int i : arr) {
+            if (i % divisor == 0) {
+                answerList.add(i);
+            }
+        }
+        Collections.sort(answerList);
+
+        if (answerList.size() == 0) {
+            return new int[]{-1};
+        }
+
+        return answerList.stream().mapToInt(i -> i).toArray();
+    }
+
+
+}
