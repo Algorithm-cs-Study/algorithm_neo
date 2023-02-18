b476f2d336178a74ed2dd19bc2cac8bc04c04c10
ffolabear <ffolabear@gmail.com>
2023-02-19 04:34:41 +0900

    feat: 정수 내림차순으로 배치하기 풀었음

---
+++ src/programmers/lv1/Solution1.java
@@ -0,0 +1,18 @@
+package programmers.lv1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Solution1 {
+
+    public long solution(long n) {
+        String[] toArray = String.valueOf(n).split("");
+        Arrays.sort(toArray, Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        for (String s : toArray) {
+            sb.append(s);
+        }
+        return Long.parseLong(sb.toString());
+    }
+
+}
