f4ebf2d3dc6e4702d8b4e9f1151a3cb488ba2b35
ffolabear <ffolabear@gmail.com>
2023-02-19 13:44:42 +0900

    feat: 두 정수의 합 풀었음

---
+++ src/programmers/lv1/Solution4.java
@@ -0,0 +1,14 @@
+package programmers.lv1;
+
+public class Solution4 {
+
+    public long solution(int a, int b) {
+
+        long answer = 0;
+        for (int i = Math.min(a, b); i <= Math.max(a, b); i++) {
+            answer += i;
+        }
+        return answer;
+    }
+
+}
